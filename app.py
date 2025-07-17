61977fe0e471fdcdd38065f983a8680075bc4a47
mestvn <20imsteven02@gmail.com>
2025-07-17 16:48:26 -0700

    app start

---
+++ app.py
@@ -0,0 +1,18 @@
+import gradio as gr
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
+def embed(text):
+    if not text.strip():
+        return "No text provided."
+    embedding = model.encode(text)
+    return embedding.tolist()
+
+api = gr.Interface(
+    fn=embed,
+    inputs=gr.Textbox(label="Enter Text"),
+    outputs=gr.JSON(label="embedding vector")
+)
+
+api.launch(show_api=True)
